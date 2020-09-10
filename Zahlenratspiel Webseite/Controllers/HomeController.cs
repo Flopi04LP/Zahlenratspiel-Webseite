ac4b58d674f6bf86ea83b6b0225dfabbb40afda5
Florian Büchi <54060167+Flopi04LP@users.noreply.github.com>
2020-09-10 17:07:22 +0200

    Initial commit

---
+++ Zahlenratspiel Webseite/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zahlenratspiel_Webseite.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            Random random = new Random();
+            int tries = 0;
+            int randomNumber = random.Next(1, 100);
+            Session.Add("zahl", randomNumber);
+            Session.Add("tries", tries);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int eingabe)
+        {
+            int tries = (int)Session.Contents["tries"];
+            int randomnumber = (int)Session.Contents["zahl"];
+            if (eingabe < 1)
+            {
+                ViewBag.ERROR = "Bitte geben sie eine Zahl zwischen 1-100 ein!";
+            }
+            if (eingabe > 100)
+            {
+                ViewBag.ERROR = "Bitte geben sie eine Zahl zwischen 1-100 ein!";
+            }
+            if (eingabe == randomnumber)
+            {
+                Session.Add("ausgabe", "Yuhuu richtig");
+                tries++;
+                Session.Contents["tries"] = tries;
+            }
+            if (eingabe > randomnumber)
+            {
+                ViewBag.ERROR = "Zahl zu gross!";
+                tries++;
+                Session.Contents["tries"] = tries;
+            }
+            if (eingabe < randomnumber)
+            {
+                ViewBag.ERROR = "Zahl zu klein!";
+                tries++;
+                Session.Contents["tries"] = tries;
+            }
+            return View();
+        }
+    }
+}
---
+++ Zahlenratspiel Webseite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Zahlenratspiel_Webseite.MvcApplication" Language="C#" %>
